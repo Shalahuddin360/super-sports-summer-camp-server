305ebe1cd9459894c07ac04a183eed9c5ded6c64
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-06-14 23:46:33 +0600

    some file are added

--- index.js
+++ index.js
@@ -167,7 +167,6 @@ async function run() {
         //deny 
         app.patch('/classes/deny/:id',async(req,res)=>{
             const id = req.params.id;
-            console.log(id)
            const query = { _id: new ObjectId(id) }
            const updateDoc = {
             $set: {
@@ -230,6 +229,8 @@ async function run() {
             const deleteResult = await selectCollection.deleteOne(query)
             res.send({insertResult,deleteResult});
         })
+
+        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
